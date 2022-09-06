d063f35048dea51c45ae3ac5f2a981a88e4133f1
dev-to2k <72263236+dev-to2k@users.noreply.github.com>
2022-09-06 11:47:12 +0700

    implement ui home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import {
-  BrowserRouter, Route, Routes
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import './App.css';
-import Home from "./pages/Home";
+import './App.css'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Content from './pages/Home/components/Content'
+import Product from './pages/Product'
 
 function App() {
   return (
-   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<Home />} />
-        {/* <Route path="teams" element={<Teams />}>
-          <Route path=":teamId" element={<Team />} />
-          <Route path="new" element={<NewTeamForm />} />
-          <Route index element={<LeagueStandings />} />
-        </Route> */}
-      </Route>
-    </Routes>
-  </BrowserRouter>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}>
+          <Route index element={<Content />} />
+          <Route path='products' element={<Product />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
+
+          {/* <Route path="teams" element={<Teams />}>
+            <Route path=":teamId" element={<Team />} />
+            <Route path="new" element={<NewTeamForm />} />
+            <Route index element={<LeagueStandings />} />
+          </Route> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home h-full absolute top-0 bottom-0 left-0 right-0 flex items-stretch'>
+      <div className='sidebar w-2/12 bg-gray-50'>
+        <Sidebar />
+      </div>
+      <div className='content w-10/12 bg-white p-4'>
+        <Outlet />
+      </div>
+    </div>
   )
 }
 
